4a4cff0e06a389afb4ea54101ab40607c738efc3
Dumba-arch <victorious651@gmail.com>
2024-09-22 10:03:25 +0300

    Add files via upload

---
+++ triangle_third_angle.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+// Function to find the third angle of a triangle
+int findThirdAngle(int angle1, int angle2) {
+    // The sum of the angles in a triangle is 180 degrees
+    return 180 - (angle1 + angle2);
+}
+
+int main() {
+    int angle1, angle2;
+    
+    cout << "Enter the first angle: ";
+    cin >> angle1;
+    
+    cout << "Enter the second angle: ";
+    cin >> angle2;
+    
+    // Calculate the third angle
+    int thirdAngle = findThirdAngle(angle1, angle2);
+    
+    cout << "The third angle is: " << thirdAngle << endl;
+    
+    return 0;
+}
+
